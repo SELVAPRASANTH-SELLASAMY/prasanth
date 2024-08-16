e8a3b94dd0a8a57233a2301fe150561b8e63b15d
prasanthsamy <selvaprasanths.20mech@kongu.edu>
2024-08-16 17:11:39 +0530

    Initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import navStyle from './navbar.module.css';
 function Navbar(){
+    const [changeBg,setChangeBg] = useState(false);
+    const scrollEffect = {
+        borderBottom :'.1rem solid rgba(255, 255, 255, 0.2)',
+        backgroundColor: '#0f172acc',
+        backdropFilter: 'blur(8px)'
+    }
+    const handleScrollEffect = () => {
+        window.scrollY > 50 ? setChangeBg(true) : setChangeBg(false);
+    }
+    useEffect(()=>{
+        window.onscroll = handleScrollEffect;
+        return () => window.removeEventListener('scroll',handleScrollEffect);
+    },[])
     return(
-        <header className={navStyle.navbar}>
+        <header style={changeBg ? scrollEffect : null} className={navStyle.navbar}>
             <h1>Prasanth</h1>
             <nav>
                 <ol>

d1c715d267d8047ae43d8d82f4f2aad80ff3fc42
Selvaprasanth-Sellasamy <prasanthsamy61@gmail.com>
2025-01-29 20:18:21 +0530

    Minor Changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -79,7 +79,7 @@ function Contact(){
                         <h5>Contact on mail</h5>
                         <address>
                             <a href="mailto:prasanthsamy61@gmail.com" target='_blank' rel='noreferrer noopener'>prasanthsamy61@gmail.com</a>
-                            <a href="mailto:selvaprasanths.20mech@kongu.edu" target='_blank' rel='noreferrer noopener'>selvaprasanths.20mech@kongu.edu</a>
+                            <a href="mailto:dvlpr.prasanth@gmail.com" target='_blank' rel='noreferrer noopener'>dvlpr.prasanth@gmail.com</a>
                         </address>
                     </div>
                 </div>
